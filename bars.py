d0a72524fa4008f1c9337330e959deb760472d21
Ruslan <avc_russel@mail.ru>
2018-01-22 17:17:59 +0700

    Create bars.py module

--- bars.py
+++ bars.py
@@ -1,21 +1,52 @@
 import json
 
+from math import sqrt
+
+import sys
+
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file:
+        return json.loads(file.read())
 
 
 def get_biggest_bar(data):
-    pass
+    max = 0
+    bar_name = ''
+    for bar in data["features"]:
+        seatsCount = bar["properties"]["Attributes"]["SeatsCount"]
+        if seatsCount > max:
+            max = seatsCount
+            bar_name = bar["properties"]["Attributes"]["Name"]
+    return bar_name
 
 
 def get_smallest_bar(data):
-    pass
+    min = 1000000000
+    bar_name = ''
+    for bar in data["features"]:
+        seatsCount = bar["properties"]["Attributes"]["SeatsCount"]
+        if seatsCount < min:
+            min = seatsCount
+            bar_name = bar["properties"]["Attributes"]["Name"]
+    return bar_name
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    path = 1000000000
+    bar_name = ''
+    for bar in data["features"]:
+        bar_coordinates = bar["geometry"]["coordinates"]
+        new_path = sqrt((bar_coordinates[0]-longitude)**2 + (bar_coordinates[1]-latitude)**2)
+        if new_path < path:
+            path = new_path
+            bar_name = bar["properties"]["Attributes"]["Name"]
+    return bar_name
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('bars.json')
+    print(get_biggest_bar(data))
+    print(get_smallest_bar(data))
+    if len(sys.argv) > 1:
+        print(get_closest_bar(data, float(sys.argv[1]), float(sys.argv[2])))
