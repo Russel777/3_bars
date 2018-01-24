9fcb3edab9035b78c97364a3340ea9c28ad88cef
Ruslan <avc_russel@mail.ru>
2018-01-25 03:26:09 +0700

    Refactoring get_biggest_bar and get_smallest_bar

--- bars.py
+++ bars.py
@@ -1,9 +1,7 @@
-import json
 import argparse
+import json
 
 from math import sqrt
-
-import sys
 
 
 def load_data(filepath):
@@ -11,24 +9,18 @@ def load_data(filepath):
         return json.loads(file.read())
 
 
-def get_biggest_bar(json_content):
-    bars_dict = {}
-    for bar in json_content["features"]:
-        bars_dict[bar["properties"]["Attributes"]["SeatsCount"]] = bar["properties"]["Attributes"]["Name"]
-    return bars_dict[max(bars_dict.keys())]
+def get_biggest_bar(bars):
+    return max(bars, key=lambda bar: bar["properties"]["Attributes"]["SeatsCount"])
 
 
-def get_smallest_bar(json_content):
-    bars_dict = {}
-    for bar in json_content["features"]:
-        bars_dict[bar["properties"]["Attributes"]["SeatsCount"]] = bar["properties"]["Attributes"]["Name"]
-    return bars_dict[min(bars_dict.keys())]
+def get_smallest_bar(bars):
+    return min(bars, key=lambda bar: bar["properties"]["Attributes"]["SeatsCount"])
 
 
-def get_closest_bar(json_content, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     path = 1000000000
     bar_name = ''
-    for bar in json_content["features"]:
+    for bar in bars:
         bar_coordinates = bar["geometry"]["coordinates"]
         new_path = sqrt((bar_coordinates[0] - longitude)**2 + (bar_coordinates[1] - latitude)**2)
         if new_path < path:
@@ -54,11 +46,15 @@ if __name__ == '__main__':
     if args.path:
         json_content = load_data(args.path)
         if args.biggest:
-            print('Biggest bar is {}'.format(get_biggest_bar(json_content)))
+            print('Biggest bar is {}'.format(
+                get_biggest_bar(json_content["features"])["properties"]["Attributes"]["Name"])
+            )
         if args.smallest:
-            print('Smallest bar is {}'.format(get_smallest_bar(json_content)))
+            print('Smallest bar is {}'.format(
+                get_smallest_bar(json_content["features"])["properties"]["Attributes"]["Name"])
+            )
         if args.longtitude and args.latitude:
-            print('Closest bar is {}'.format(get_closest_bar(json_content, args.longtitude, args.latitude)))
+            print('Closest bar is {}'.format(get_closest_bar(json_content["features"], args.longtitude, args.latitude)))
         elif args.longtitude or args.latitude:
             print('Not enough parameters')
     else:
