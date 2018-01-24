17baa9bab4b93ffeab7f8dc459ca7c52a40ee6d3
Ruslan <avc_russel@mail.ru>
2018-01-25 02:59:21 +0700

    Add argparse module

--- bars.py
+++ bars.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 from math import sqrt
 
@@ -37,8 +38,28 @@ def get_closest_bar(json_content, longitude, latitude):
 
 
 if __name__ == '__main__':
-    json_content = load_data('bars.json')
-    print('Biggest bar is {}'.format(get_biggest_bar(json_content)))
-    print('Smallest bar is {}'.format(get_smallest_bar(json_content)))
-    if len(sys.argv) > 1:
-        print('Closest bar is {}'.format(get_closest_bar(json_content, float(sys.argv[1]), float(sys.argv[2]))))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", help="path to the file")
+    parser.add_argument("--biggest", help="display a biggest bar in file list", action="store_true")
+    parser.add_argument("--smallest", help="display a smallest bar in file list", action="store_true")
+    parser.add_argument("-x",
+                        "--longtitude",
+                        help="input longitude and latitude for display a closest bar in file list ",
+                        type=float)
+    parser.add_argument("-y",
+                        "--latitude",
+                        help="input longitude and latitude for display a closest bar in file list ",
+                        type=float)
+    args = parser.parse_args()
+    if args.path:
+        json_content = load_data(args.path)
+        if args.biggest:
+            print('Biggest bar is {}'.format(get_biggest_bar(json_content)))
+        if args.smallest:
+            print('Smallest bar is {}'.format(get_smallest_bar(json_content)))
+        if args.longtitude and args.latitude:
+            print('Closest bar is {}'.format(get_closest_bar(json_content, args.longtitude, args.latitude)))
+        elif args.longtitude or args.latitude:
+            print('Not enough parameters')
+    else:
+        print("Enter path")
