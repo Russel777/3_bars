b793d7159c75d2edfd3d7071105ae97b9f57c404
Ruslan <avc_russel@mail.ru>
2018-01-26 15:37:26 +0700

    Add required parameter to path

--- bars.py
+++ bars.py
@@ -28,9 +28,10 @@ def get_closest_bar(bars, longitude, latitude):
             bar_name = bar["properties"]["Attributes"]["Name"]
     return bar_name
 
+
 def arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", help="path to the file")
+    parser.add_argument("-p", "--path", help="path to the file", required=True)
     parser.add_argument("--biggest", help="display a biggest bar in file list", action="store_true")
     parser.add_argument("--smallest", help="display a smallest bar in file list", action="store_true")
     parser.add_argument("-x",
@@ -42,22 +43,21 @@ def arguments():
                         help="input longitude and latitude for display a closest bar in file list ",
                         type=float)
     args = parser.parse_args()
-    if args.path:
-        json_content = load_data(args.path)
-        if args.biggest:
-            print('Biggest bar is {}'.format(
-                get_biggest_bar(json_content["features"])["properties"]["Attributes"]["Name"])
-            )
-        if args.smallest:
-            print('Smallest bar is {}'.format(
-                get_smallest_bar(json_content["features"])["properties"]["Attributes"]["Name"])
-            )
-        if args.longtitude and args.latitude:
-            print('Closest bar is {}'.format(get_closest_bar(json_content["features"], args.longtitude, args.latitude)))
-        elif args.longtitude or args.latitude:
-            print('Not enough parameters')
-    else:
-        print("Enter path")
+
+    json_content = load_data(args.path)
+    if args.biggest:
+        print('Biggest bar is {}'.format(
+            get_biggest_bar(json_content["features"])["properties"]["Attributes"]["Name"])
+        )
+    if args.smallest:
+        print('Smallest bar is {}'.format(
+            get_smallest_bar(json_content["features"])["properties"]["Attributes"]["Name"])
+        )
+    if args.longtitude and args.latitude:
+        print('Closest bar is {}'.format(get_closest_bar(json_content["features"], args.longtitude, args.latitude)))
+    elif args.longtitude or args.latitude:
+        print('Not enough parameters')
+
 
 if __name__ == '__main__':
     arguments()
